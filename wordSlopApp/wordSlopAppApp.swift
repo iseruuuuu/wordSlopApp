a5ec3afbd23c01ed0c320aa3a89aeeba9695175e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-22 18:09:07 +0900

    Initial Commit

---
+++ wordSlopApp/wordSlopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordSlopAppApp.swift
+//  wordSlopApp
+//
+//  Created by user on 2020/10/22.
+//
+
+import SwiftUI
+
+@main
+struct wordSlopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
